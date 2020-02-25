18c46a3e8a287fa78744774ae64f942b16c9a676
Colton stephens <stephens.colton@icloud.com>
2020-02-25 01:41:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,10 +27,11 @@ def using_flatten(array)
 end
 
 def using_delete_at(array, string)
-  array.delete(string)
+  array.delete_at(string)
 end
 
 def using_delete(array, string)
-  array.
+  array.delete(string)
+end
 
 
